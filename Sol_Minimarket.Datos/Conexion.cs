79284aac4bb89db8a29507598298683eb88e2dc7
ValeriaBaltazar19 <valerializethbaltazarzapata@gmail.com>
2025-04-26 15:15:25 -0500

    Agregar archivos de proyecto.

---
+++ Sol_Minimarket.Datos/Conexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Sol_Minimarket.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "BD_MINIMARKET";
+            this.Servidor = "DESKTOP-5M827A4"; //Cambiar el nombre del servidor de tu cp, lo encuentras en propiedades del sqlserver
+            this.Usuario = "sistemas"; //Este usuario lo creas en el sqlserver en la carpeta seguridad(inicio de sesión)
+            this.Clave = "soporte";
+            this.Seguridad = false;
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.Base + ";";
+                if (Seguridad)
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security = SSPI";
+                }
+                else
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "User Id=" + this.Usuario + "; Password=" + this.Clave;
+                }
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
+
